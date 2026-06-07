2bf1e403f631d4e1ea39361197327cc92b5af4b2
ian16munoz3nunez1 <ianemn1603@gmail.com>
2026-06-06 18:13:46 -0600

    hlsearch enabled and neogit open split added

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,7 +6,15 @@ return {
         "nvim-telescope/telescope.nvim"
     },
     config = function()
+        local neogit = require('neogit')
         vim.keymap.set("n", "<leader>ng", ":Neogit<cr>", {})
+        --[[
+        vim.keymap.set("n",
+            "<leader>ng",
+            function() neogit.open({ kind = "vsplit" }) end,
+            { desc = "Open NeoGit UI" }
+        )
+        ]]--
     end
 }
 
