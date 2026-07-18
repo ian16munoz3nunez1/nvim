cb7a2de2f03c654ca4391c1a34c53581301de0ee
ian16munoz3nunez1 <ianemn1603@gmail.com>
2026-07-18 02:05:03 -0600

    New features and settings added to treesitter.lua
    - ensure_installed setting added to file
    - highlight setting added to file
    - indent setting added to file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,19 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = 'master',
     lazy = false,
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "lua", "vim", "vimdoc", "query", "bash", "c", "python", "html", "css", "javascript"
+            },
+
+            sync_install = false,
+
+            highlight = { enable = true },
+
+            indent = { enable = true }
+        })
+    end
 }
 
